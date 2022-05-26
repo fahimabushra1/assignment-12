5f70aceb00429be277e69ffc7fdcb50ee6b53a7c
fahimabushra1 <bushra.rifeen@gmail.com>
2022-05-26 08:46:51 +0600

    banner create

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -1,11 +1,11 @@
 import React from "react";
+import car from '../../../Images/car.jpg'
 
-const Banner=()=>{
-    return(
+const Banner = () => {
+    return (
         <div>
-
+            <img className="w-screen" src={car}></img>
         </div>
     )
 }
-
 export default Banner;
--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,14 +1,18 @@
 import React from "react";
+import Banner from "../Banner/Banner";
 import Header from "../../Shared/Header/Header";
 import Products from "../Products/Products";
 import Summery from "../Summery/Summery";
+import Footer from "../../Shared/Footer/Footer";
 
 const Home = () => {
     return (
         <div>
             <Header></Header>
+            <Banner></Banner>
             <Summery></Summery>
             <Products></Products>
+            <Footer></Footer>
         </div>
 
     )
