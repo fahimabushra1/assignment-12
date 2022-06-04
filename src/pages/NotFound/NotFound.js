39b36331ea975fa8d10e2217ba2638f869d1acd4
fahimabushra1 <bushra.rifeen@gmail.com>
2022-06-04 18:49:53 +0600

    notFound

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,9 +1,10 @@
 import React from "react";
+import error from "../../Images/404.jpg"
 
-const NotFound=()=>{
-    return(
-        <div>
-
+const NotFound = () => {
+    return (
+        <div className="w-screen">
+            <img src={error}></img>
         </div>
     )
 }
