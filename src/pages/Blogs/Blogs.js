8546674417f7aa0b62c078e55cacbf68ada1e493
fahimabushra1 <bushra.rifeen@gmail.com>
2022-06-16 10:26:20 +0600

    blogs question writting

--- src/pages/Blogs/Blogs.js
+++ src/pages/Blogs/Blogs.js
@@ -1,9 +1,18 @@
 import React from "react";
 
-const Blogs=()=>{
-    return(
-        <div>
-
+const Blogs = () => {
+    return (
+        <div className="text-blue-700 m-10">
+            <h3 className="font-semibold text-xl my-2">Que: How will you improve the performance of a React Application?</h3>
+            <p className="font-semibold">Ans:React</p>
+            <h3 className="font-semibold text-xl my-2">What are the different ways to manage a state in a React application?</h3>
+            <p className="font-semibold">Ans:React</p>
+            <h3 className="font-semibold text-xl my-2">Que: How does prototypical inheritance work?</h3>
+            <p className="font-semibold">Ans:React</p>
+            <h3 className="font-semibold text-xl my-2">Que: What is a unit test? Why should write unit tests?</h3>
+            <p className="font-semibold">Ans:React</p>
+            <h3 className="font-semibold text-xl my-2">Que: Why you do not set the state directly in React. For example, if you have const [products, setProducts] = useState([]). Why you do not set products = [...] instead, you use the setProducts</h3>
+            <p className="font-semibold">Ans:React</p>
         </div>
     )
 }
